c829af88f2fbef2d1bc89b1dd468dbe1e2280464
CheralaHarshini <2403a52300@sru.edu.in>
2026-02-22 22:39:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+window.onload = function() {
+
+    const ctx = document.getElementById('performanceChart');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Math', 'Physics', 'Chemistry'],
+            datasets: [{
+                label: 'Score %',
+                data: [80, 60, 90]
+            }]
+        }
+    });
+}
